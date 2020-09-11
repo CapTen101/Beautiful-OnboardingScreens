4240b798df0edcf33f0256819e46d63a2d6759c9
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-09-11 18:46:52 +0530

    added mockito library for unit testing

---
+++ app/src/androidTest/java/com/example/onboardingfold/MainActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.onboardingfold;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class MainActivityTest {
+    @Test
+    public void testMainActivity() {
+
+    }
+}
